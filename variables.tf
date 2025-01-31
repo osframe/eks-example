5b776fede3708811faf763a4a922863f66d68996
osframe <67242709+osframe@users.noreply.github.com>
2025-01-31 14:47:00 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  default     = "MyEKSProject"
+}
